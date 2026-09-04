4184ebccb75a087aba64c9877c4daaeeb0f78e21
website <tejnoors79@gmail.com>
2026-09-04 12:02:39 +0530

    Delete frontend directory

--- frontend/app/resume-analyzer/layout.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardShellLayout from "@/components/layout/DashboardShellLayout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DashboardShellLayout>{children}</DashboardShellLayout>;
-}
--- frontend/app/settings/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ProtectedShellLayout from "@/components/layout/ProtectedShellLayout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ProtectedShellLayout>{children}</ProtectedShellLayout>;
-}
